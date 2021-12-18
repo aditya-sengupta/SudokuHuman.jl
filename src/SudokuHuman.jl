a8303cd576a779ddd72e1be082d7bbe82a03e1cf
aditya-sengupta <aditya.sengupta@berkeley.edu>
2021-12-18 22:36:10 +0000

    general solving but huge allocations

--- src/SudokuHuman.jl
+++ src/SudokuHuman.jl
@@ -5,6 +5,7 @@ A Sudoku solver whose steps are transparent to humans!
 module SudokuHuman
     using StaticArrays
     using LinearAlgebra
+    using ProgressMeter
     import Base: show, eachrow, eachcol, copy, print
     import Base.Iterators: product, drop
 
@@ -107,6 +108,7 @@ module SudokuHuman
             for (i, subset) in enumerate(gen(flags))
                 checkrow = vec(sum(subset, dims=1)) .== 0
                 checkcol = vec(sum(subset, dims=2)) .== 0
+
                 if any(checkrow)
                     inds = findall(checkrow)
                     throw("Inconsistent state in $desc $i: indices $inds have no possible values")
@@ -119,7 +121,7 @@ module SudokuHuman
         end
     end
 
-    function solve(s::Sudoku)
+    function solve(s::Sudoku; depth=0, maxdepth=3)
         solution = copy(s)
         flags = @MArray ones(Bool, 9, 9, 9)
         for index in product(1:9, 1:9)
@@ -129,53 +131,84 @@ module SudokuHuman
         end
 
         last_num_found = sum(vec(solution.grid) .> 0)
-        try
-            while last_num_found < 81
-                # first check: for each square, is there only one possible answer?
-                for index in product(1:9, 1:9)
-                    slice = flags[index..., 1:9] # look at all the possible values "index" could take on
-                    if sum(slice) == 1 && solution.grid[index...] == 0 # I know something and I didn't know it before this
-                        val = findfirst(slice)
-                        println("$index can only be $val")
-                        solution.grid[index...] = val
-                        update!(solution, flags, index) # eliminate possibilities from new information
-                    end
+        while last_num_found < 81
+            # first check: for each square, is there only one possible answer?
+            for index in product(1:9, 1:9)
+                slice = flags[index..., 1:9] # look at all the possible values "index" could take on
+                if sum(slice) == 1 && solution.grid[index...] == 0 # I know something and I didn't know it before this
+                    val = findfirst(slice)
+                    # println("$index can only be $val")
+                    solution.grid[index...] = val
+                    update!(solution, flags, index) # eliminate possibilities from new information
                 end
-
-                # second check: for each (row, column, block), is there only one possible place a certain value could go?
-                for (desc, gen) in zip(("row", "col", "block"), (eachrow, eachcol, eachblock))
-                    for (num, (possibilities, grid_vector)) in enumerate(zip(gen(flags), gen(solution.grid)))
-                        # "possibilities" is a 9x9: (i, j) is "could position i in the row (resp. column, block) be value j?"
-                        # grid_vector is a length-9 vector with the known values
-                        vacancies = findall(x -> x == 0, grid_vector) # all the indices we need to fill in
-                        newlocs = Dict{Int64,Int64}()
-                        fishing = vec(sum(possibilities, dims=1))
-                        for k in 1:9
-                            if fishing[k] == 1 && !(k in vacancies)
-                                pos = findfirst(x -> x == 1, possibilities[:,k])
-                                newlocs[pos] = k
-                            end
-                        end
-                        if length(newlocs) > 0
-                            for n in keys(newlocs)
-                                val = newlocs[n]
-                                println("$desc $num's only possible location for $val is $n")
-                                grid_vector[n] = val                                
-                                update!(solution, flags, indexof(desc, num, n)) # eliminate possibilities from new information
-                            end
-                        end
-                    end
-                end
-
-                if sum(vec(solution.grid) .> 0) == last_num_found # we didn't find anything new this iteration
-                    return solution, flags
-                end
-                last_num_found = sum(vec(solution.grid) .> 0)
             end
-        catch err
-            println(err)
-            return solution, flags
+
+            # second check: for each (row, column, block), is there only one possible place a certain value could go?
+            for (desc, gen) in zip(("row", "col", "block"), (eachrow, eachcol, eachblock))
+                for (num, (possibilities, grid_vector)) in enumerate(zip(gen(flags), gen(solution.grid)))
+                    # "possibilities" is a 9x9: (i, j) is "could position i in the row (resp. column, block) be value j?"
+                    # grid_vector is a length-9 vector with the known values
+                    vacancies = findall(x -> x == 0, grid_vector) # all the indices we need to fill in
+                    newlocs = Dict{Int64,Int64}()
+                    fishing = vec(sum(possibilities, dims=1))
+                    for k in 1:9
+                        if fishing[k] == 1 && !(k in vacancies)
+                            pos = findfirst(x -> x == 1, possibilities[:,k])
+                            newlocs[pos] = k
+                        end
+                    end
+                    if length(newlocs) > 0
+                        for n in keys(newlocs)
+                            val = newlocs[n]
+                            # println("$desc $num's only possible location for $val is $n")
+                            grid_vector[n] = val                                
+                            update!(solution, flags, indexof(desc, num, n)) # eliminate possibilities from new information
+                        end
+                    end
+                end
+            end
+
+            if sum(vec(solution.grid) .> 0) == last_num_found # we didn't find anything new this iteration
+                 if depth < maxdepth
+                    return solve_guess(solution, flags, depth, maxdepth)
+                 else
+                    return solution
+                 end
+            end
+            last_num_found = sum(vec(solution.grid) .> 0)
         end
-        solution, flags
+        @assert check(solution; verbose=false)
+        # the only way you should get here without causing an error further up should be if you've got a completed puzzle
+        solution
+    end
+
+    function solve_guess(s::Sudoku, flags::AbstractArray{Bool, 3}, depth, maxdepth)
+        if check(s; verbose=false)
+            return s
+        end
+        nums_poss = vec(sum(flags, dims=3)[:,:,1])
+        where_to_guess = sortperm(nums_poss)
+        start_sort = findfirst(x -> x == findfirst(nums_poss .== minimum(nums_poss[nums_poss .> 1])), where_to_guess)
+        where_to_guess = where_to_guess[start_sort:end]
+        guessed_sudoku = copy(s)
+        for idx in where_to_guess
+            i, j = (idx - 1) % 9 + 1, ((idx - 1) ÷ 9) + 1
+            possible_guesses = findall(flags[i, j, :])
+            if length(possible_guesses) < 2
+                continue # hack, you shouldn't get this, but I am
+            end
+            for guess in possible_guesses             
+                guessed_sudoku.grid[i, j] = guess
+                try
+                    solution = solve(guessed_sudoku; depth=depth+1, maxdepth=maxdepth)
+                    if check(solution; verbose=false)
+                        return solution 
+                    end
+                catch 
+                    guessed_sudoku.grid[i, j] = 0
+                end
+            end
+        end
+        throw("Ran through all guesses to depth=$maxdepth without reaching a solution.")
     end
 end
