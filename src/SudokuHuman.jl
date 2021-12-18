ef67199f5d7439803d7dc8f35aefc87b6a82e5eb
aditya-sengupta <aditya.sengupta@berkeley.edu>
2021-12-18 17:27:14 +0000

    don't worry i made it worse :)

--- src/SudokuHuman.jl
+++ src/SudokuHuman.jl
@@ -6,6 +6,7 @@ module SudokuHuman
     using StaticArrays
     using LinearAlgebra
     import Base.show, Base.eachrow, Base.eachcol, Base.copy
+    import Base.Iterators: product
 
     struct Sudoku
         grid::MMatrix{9,9,Int64}
@@ -17,6 +18,11 @@ module SudokuHuman
     export check, possibilities, update, solve, print
 
     topleft(i) = 3 * ((i - 1) ÷ 3) + 1
+
+    function blockof(i, j)
+        tli, tlj = topleft(i), topleft(j)
+        return (tli:(tli+2), tlj:(tlj+2))
+    end
 
     function Base.show(io::IO, s::Sudoku)
         println("   -----    -----    -----")
@@ -37,9 +43,15 @@ module SudokuHuman
 
     Base.eachrow(s::Sudoku) = Base.eachrow(s.grid)
     Base.eachcol(s::Sudoku) = Base.eachcol(s.grid)
-    eachblock(m::AbstractMatrix) = (vec(m[x...]) for x in (((3 * (i ÷ 3) + 1):(3 * (i ÷ 3) + 3), (3 * (i % 3) + 1):(3 * (i % 3) + 3)) for i in 0:8))
+    Base.eachrow(arr::AbstractArray{T, 3} where T) = eachslice(arr, dims=1)
+    Base.eachcol(arr::AbstractArray{T, 3} where T) = eachslice(arr, dims=2)
+    blockindsof(i) = ((3 * ((i-1) ÷ 3) + 1):(3 * ((i-1) ÷ 3) + 3), (3 * ((i-1) % 3) + 1):(3 * ((i-1) % 3) + 3))
+    blockinds() = (blockindsof(i) for i in 1:9)
+    eachblock(arr::AbstractArray{T, 3} where T) = (reshape(arr[x..., :], (9,9)) for x in blockinds())
+    eachblock(m::AbstractMatrix) = (vec(m[x...]) for x in blockinds())
+
     eachblock(s::Sudoku) = eachblock(s.grid)
-    Base.copy(s::Sudoku) = Sudoku(copy(s.grid))
+    Base.copy(s::Sudoku) = Sudoku(Base.copy(s.grid))
 
     function check(block, type; verbose=true)
         if length(Set(block)) < 9
@@ -72,62 +84,74 @@ module SudokuHuman
         solved
     end
 
-    function possibilities(s::Sudoku, i, j)
-        if s.grid[i,j] != 0
-            return [s.grid[i,j]]
+    function indexof(desc::String, iternum::Int64, pos::Int64)
+        if desc == "row"
+            return (iternum, pos)
+        elseif desc == "col"
+            return (pos, iternum)
+        elseif desc == "block"
+            blockinds = blockindsof(iternum)
+            return first(drop(product(blockinds...), pos - 1))
         end
-        values = 1:9
-        values = setdiff(values, s.grid[i,:])
-        values = setdiff(values, s.grid[:,j])
-        tli, tlj = topleft(i), topleft(j)
-        values = setdiff(values, s.grid[tli:tli+2, tlj:tlj+2])
-        values
     end
 
-    function update(s::Sudoku)
-        change = false
-        for i in 1:9
-            for j in 1:9
-                if s.grid[i, j] == 0
-                    poss = possibilities(s, i, j)
-                    if length(poss) == 1
-                        s.grid[i,j] = poss[1]
-                        change = true
-                    end
-                end
-            end
+    function update!(s::Sudoku, flags::AbstractArray{Bool, 3}, index::Tuple{Int64,Int64})
+        val = s.grid[index...]
+        if val > 0
+            flags[index[1], 1:9, val] .= false # nothing in the row can be 'val'
+            flags[1:9, index[2], val] .= false # nothing in the col can be 'val'
+            flags[blockof(index...)..., val] .= false # nothing in the block can be 'val'
+            flags[index..., val] = true # except for the value we just found
         end
-
-        possibilities_matrix = hcat([[possibilities(s, i, j) for i in 1:9] for j in 1:9]...)
-
-        for gen in (Base.eachrow, Base.eachcol, eachblock)
-            for (possibilities_vector, grid_vector) in zip(gen(possibilities_matrix), gen(s.grid))
-                fishing = [findall(k in x for x in possibilities_vector) for k in 1:9] # indices where the value at index could be
-                knowns = Dict{Int64,Int64}() # maps index to its known value
-                for (l, f) in enumerate(fishing)
-                    if length(f) == 1
-                        knowns[f[1]] = l
-                    end
-                end
-                vacancies = findall(x -> x == 0, grid_vector) # all the indices we need to fill in
-                newlocs = intersect(vacancies, keys(knowns))
-                if length(newlocs) > 0
-                    for n in newlocs
-                        grid_vector[n] = knowns[n]
-                        change = true
-                    end
-                end
-            end
-        end
-
-        change
     end
 
     function solve(s::Sudoku)
-        change = true
         solution = Base.copy(s)
-        while change
-            change = update(solution)
+        flags = @MArray ones(Bool, 9, 9, 9)
+        for index in product(1:9, 1:9)
+            update!(solution, flags, index)
+        end
+
+        num_found = sum(vec(solution.grid) .> 0)
+        last_num_found = sum(vec(solution.grid) .> 0)
+        while num_found < 81
+            # first check: for each square, is there only one possible answer?
+            for index in product(1:9, 1:9)
+                slice = flags[index..., 1:9] # look at all the possible values "index" could take on
+                if sum(slice) == 1 && solution.grid[index...] == 0 # I know something and I didn't know it before this
+                    val = findfirst(slice)
+                    println("$index can only be $val")
+                    solution.grid[index...] = val
+                    update!(solution, flags, index) # eliminate possibilities from new information
+                    num_found += 1
+                end
+            end
+
+            # second check: for each (row, column, block), is there only one possible place a certain value could go?
+            for (desc, gen) in zip(("row", "col", "block"), (Base.eachrow, Base.eachcol, eachblock))
+                for (num, (possibilities, grid_vector)) in enumerate(zip(gen(flags), gen(solution.grid)))
+                    # "possibilities" is a 9x9: (i, j) is "could position i in the row (resp. column, block) be value j?"
+                    # grid_vector is a length-9 vector with the known values
+                    vacancies = findall(x -> x == 0, grid_vector) # all the indices we need to fill in
+                    knowns = findall(x -> x == 1, sum(possibilities, dims=1)) # all the indices we know
+                    newlocs = intersect(vacancies, knowns) # where's the new information
+                    if length(newlocs) > 0
+                        for n in newlocs
+                            val = findfirst(x -> x == 1, possibilities[n,:])
+                            println("$desc $num's only possible location for $val is $n")
+                            grid_vector[n] = findfirst(x -> x == 1, possibilities[n,:])
+                            update!(s, flags, indexof(desc, num, n))
+                            num_found += 1
+                        end
+                    end
+                end
+            end
+
+            if num_found == last_num_found # we didn't find anything new this iteration
+                return solution
+            end
+            last_num_found = num_found
+            num_found = sum(vec(solution.grid) .> 0)
         end
         solution
     end
